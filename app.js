d527c20622350564727ca1ae38b14f5913c1a556
Sonia Pepe <sonia.pepe@fdvsolutions.com>
2015-06-17 17:15:04 -0300

    Index not rendering

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('hello express!');
+    response.send('hello express!!!');
 });
 
 app.listen(8989, function(){
