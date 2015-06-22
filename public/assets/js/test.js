e186257618b9a5dfe28f495f07689915d51a24f8
Sonia Pepe <sonia.pepe@fdvsolutions.com>
2015-06-22 15:17:44 -0300

    Added an ajax prefilter request...still need to understand how does it work.

--- public/assets/js/test.js
+++ public/assets/js/test.js
@@ -1,12 +1,24 @@
+$.ajaxPrefilter(function (options, originalOptions, jqXHR) {
+    options.url = 'http://localhost:8989' + options.url;
+});
+
 var UserList = Backbone.View.extend({
     el: '.page',
     render: function(){
-        this.$el.html('Content should show here!');
+        var users = new Users();
+        users.fetch({
+            success: function () {
+                this.$el.html('Content should show here!');
+            }
+        });
     }
 });
 
 var userList = new UserList();
 
+var Users = Backbone.Collection.extend({
+   url: '/users'
+});
 
 var Router = Backbone.Router.extend({
     routes: {
